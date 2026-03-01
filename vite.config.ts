287ae49712bf0e5f5297b0cca789937e6042d3e5
239aditya <adityasaini239@gmail.com>
2026-03-01 13:51:36 +0530

    fixed vercel blank page

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  base: "/Resume_website_v2/",
+  base: "/",
   plugins: [react()],
   resolve: {
     alias: {
